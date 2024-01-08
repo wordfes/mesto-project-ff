0d032850d391544cf12422b14901d7283e63ed64
Albert Safin <wordfes@yandex.ru>
2024-01-08 18:44:47 +0500

    Пятый проект 1.2

--- scripts/index.js
+++ scripts/index.js
@@ -6,16 +6,16 @@ const cardsContainer = document.querySelector(".places__list");
 function createCard(card) {
   const cardElement = cardTemplate.cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  const thisCard = cardElement.querySelector(".card");
+  const currentCard = cardElement.querySelector(".card");
   cardElement.querySelector(".card__title").textContent = card.name;
   cardElement.querySelector(".card__image").src = card.link;
   cardElement.querySelector(".card__image").alt = card.name;
-  deleteButton.addEventListener("click", () => deleteCard(thisCard));
+  deleteButton.addEventListener("click", () => deleteCard(currentCard));
   return cardElement;
 }
 // @todo: Функция удаления карточки
-function deleteCard(thisCard) {
-  thisCard.remove();
+function deleteCard(currentCard) {
+  currentCard.remove();
 } 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((card) => {
